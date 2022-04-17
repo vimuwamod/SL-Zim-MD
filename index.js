e0f3565b615376ccef8a69b5fb4e2259923857ca
DRIPS MEMES BLENNIE BLESSED TUNA <100164866+zim-bot@users.noreply.github.com>
2022-04-17 13:21:19 +0200

    ZIM BOT INC-MD

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ Description: ${metadata.desc}
                     ZimBotInc.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `@${num.split("@")[0]} 𝗸𝗶𝗰𝗸𝗲𝗱 𝗼𝘂𝘁 𝗼𝗳 ${metadata.subject}
 
  '𝗕𝘆𝗲 𝗺𝘆 𝗹𝗼𝘃𝗲𝗹𝘆 𝗳𝗿𝗶𝗲𝗻𝗱 𝗗𝗼𝗻𝘁 𝗰𝗼𝗺𝗲 𝗯𝗮𝗰𝗸 𝗵𝗲𝗿𝗲' })
-                }} else {
+                } else {
                                                                                                          let buttons = [
                                                                                                                 { buttonId: '', buttonText: { displayText: '𝗪𝗘𝗟𝗖𝗢𝗠𝗘 𝗖𝗛𝗢𝗠𝗜𝗘' }, type: 1 },
                                                                                                                 { buttonId: '', buttonText: { displayText: '𝗙𝗘𝗘𝗟 𝗔𝗧 𝗛𝗢𝗠𝗘' }, type: 1 }
